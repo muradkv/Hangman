47d2fe424fa94af390ade9d240f0c2af6ac882e8
murad <muradkv@gmail.com>
2019-05-18 20:29:24 +0300

    Add labels, textfield and button to check user answer

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var word: UILabel!
+    @IBOutlet weak var inputLetter: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func checkTapped(_ sender: Any) {
 
-
+    }
 }
 

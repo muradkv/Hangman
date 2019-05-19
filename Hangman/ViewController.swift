1b87187acac570859bf3bdf29ed74e89d06fcc44
murad <muradkv@gmail.com>
2019-05-19 23:32:29 +0300

    Add startGame func that choose random word from elements of array

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -12,13 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var word: UILabel!
     @IBOutlet weak var inputLetter: UITextField!
     
+    let wordsArray = ["hello", "apple", "computer", "cat"]
+    var currentWord = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        startGame()
+        
+        word.text = currentWord
+        
     }
     
     @IBAction func checkTapped(_ sender: Any) {
-
+    }
+    
+    func startGame() {
+        if let currentWord = wordsArray.randomElement() {
+            self.currentWord = currentWord
+        }
     }
 }
 
